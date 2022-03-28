46852d7971183829b0907c8986d8d76176f7154b
NaderYouhanna <naderyouhanna@gmail.com>
2022-03-28 23:55:44 +0200

    Improved layout of Login

--- android_app/lib/Log In/login.dart
+++ android_app/lib/Log In/login.dart
@@ -5,102 +5,109 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      //mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        const SizedBox(
-          height: 35,
-        ),
-        Row(
-          children: <Widget>[
-            const SizedBox(
-              width: 10,
-            ),
-            const Icon(
-              Icons.arrow_back,
-              size: 29,
-            ),
-            const SizedBox(
-              width: 80,
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(18),
-              child: Image.asset(
-                'assets/images/logo_icon.png',
-                width: 120,
-                height: 50,
-                fit: BoxFit.fill,
+    return SingleChildScrollView(
+      child: Column(
+        //mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const SizedBox(
+            height: 35,
+          ),
+          Row(
+            children: <Widget>[
+              const SizedBox(
+                width: 10,
+              ),
+              const Icon(
+                Icons.arrow_back,
+                size: 29,
+              ),
+              const SizedBox(
+                width: 80,
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(18),
+                child: Image.asset(
+                  'assets/images/logo_icon.png',
+                  width: 120,
+                  height: 50,
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 30),
+          const SizedBox(
+            width: 320,
+            child: Text(
+              'To get started, first enter your phone, email, or @username',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontFamily: 'RalewayMedium',
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ],
-        ),
-        const SizedBox(height: 30),
-        const SizedBox(
-          width: 320,
-          child: Text(
-            'To get started, first enter your phone, email, or @username',
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              fontFamily: 'RalewayMedium',
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-            ),
           ),
-        ),
-        const SizedBox(height: 20),
-        const SizedBox(
-          width: 330,
-          child: TextField(
-            //controller: emailController,
-            decoration: InputDecoration(
-              labelText: 'Phone, email, or username',
-            ),
-          ),
-        ),
-        const SizedBox(height: 400),
-        Row(
-          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            const SizedBox(width: 12),
-            ElevatedButton(
-              child: const Text(
-                'Forgot Password?',
+          const SizedBox(height: 20),
+          SizedBox(
+            width: 330,
+            child: TextField(
+              decoration: const InputDecoration(
+                labelText: 'Phone, email or username',
               ),
-              onPressed: () {},
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                shape: MaterialStateProperty.all<StadiumBorder>(
-                  const StadiumBorder(
-                    side: BorderSide(
-                      color: Colors.grey,
-                      width: 1,
+              controller: emailController,
+            ),
+          ),
+          const SizedBox(height: 400),
+          Row(
+            children: <Widget>[
+              const SizedBox(width: 12),
+              ElevatedButton(
+                child: const Text(
+                  'Forgot Password?',
+                ),
+                onPressed: () {},
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  shape: MaterialStateProperty.all<StadiumBorder>(
+                    const StadiumBorder(
+                      side: BorderSide(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(width: 125),
-            ElevatedButton(
-              child: const Text('Next'),
-              onPressed: () {},
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.grey),
-                shape: MaterialStateProperty.all<StadiumBorder>(
-                  const StadiumBorder(
-                    side: BorderSide(
-                      color: Colors.black,
-                      width: 1,
-                    ),
-                  ),
-                ),
+              const SizedBox(width: 125),
+              ElevatedButton(
+                child: const Text('Next'),
+                onPressed: () {},
+                style: (emailController.text.isEmpty)
+                    ? ButtonStyle(
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                            Colors.grey.shade400),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.grey.shade600),
+                        shape: MaterialStateProperty.all<StadiumBorder>(
+                          const StadiumBorder(),
+                        ),
+                      )
+                    : ButtonStyle(
+                        foregroundColor:
+                            MaterialStateProperty.all<Color>(Colors.white),
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.black),
+                      ),
               ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
