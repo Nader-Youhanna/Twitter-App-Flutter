17d0b0d8b9dd67bf94db0a2894a44a6dcdccaafc
Nader-Youhanna <naderyouhanna@gmail.com>
2022-03-31 00:40:08 +0200

    Added floating action button

--- android_app/lib/widgets/Messages/inbox.dart
+++ android_app/lib/widgets/Messages/inbox.dart
@@ -18,11 +18,26 @@ class _InboxState extends State<Inbox> {
             color: Colors.black,
             onPressed: () => {}), //button should open to side bar,
         actions: [
+          Container(
+            width: 250,
+            child: const TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Search Direct Messages',
+              ),
+            ),
+          ),
+
           IconButton(
               icon: const Icon(Icons.settings_outlined),
               color: Colors.black,
               onPressed: () => {}), //button shoud direct to setings
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        // button should open the what's happening page
+        onPressed: () => {},
+        child: const Icon(Icons.add),
       ),
     );
   }
