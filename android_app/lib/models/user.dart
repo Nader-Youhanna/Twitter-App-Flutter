c50da984740f5fb181d68973e0dfd52024c89477
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-04-01 00:12:29 +0200

    Update user.dart

--- android_app/lib/models/user.dart
+++ android_app/lib/models/user.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
 class User {
-  final username;
+  final String uid;
+  final List following;
+  final List followers;
+  final String username;
+  final String photoUrl;
+  final String bio;
   final password;
   final email;
   final phone;
   bool loggedIn = false;
-  User({@required this.username, this.email, this.password, this.phone}) {}
+  User({
+    required this.username,
+    this.email,
+    this.password,
+    this.phone,
+    required this.uid,
+    required this.bio,
+    required this.followers,
+    required this.following,
+    required this.photoUrl,
+  });
+
+  Map<String, dynamic> toJson() => {
+        "username": username,
+        "uid": uid,
+        "emial": email,
+        "photoUrl": photoUrl,
+        "bio": bio,
+        "followers": followers,
+        "following": following,
+        "password": password,
+        "phone": phone,
+      };
 }
