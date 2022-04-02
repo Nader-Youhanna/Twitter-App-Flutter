80e0b2cc10b0d50bbf52a47e57fbe5bea464abec
Nader-Youhanna <naderyouhanna@gmail.com>
2022-04-02 16:18:39 +0200

    Added AppBar to timeline

--- android_app/lib/widgets/timeline.dart
+++ android_app/lib/widgets/timeline.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import './Tweets/tweet.dart';
+import 'package:android_app/widgets/user_profile/profile.dart';
 
 class Timeline extends StatefulWidget {
   final List<Tweet> tweets;
@@ -15,17 +15,62 @@ class _TimelineState extends State<Timeline> {
   final appBarHeight = 100.0;
   final bottomNavigationBarHeight = 100.0;
 
+  final _appBarText = 'Search';
+  void _goToUserProfile(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(builder: (_) {
+        return Profile();
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     var mediaQuery = MediaQuery.of(context);
 
-    return Container(
-      height: mediaQuery.size.height,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return widget.tweets[index];
-        },
-        itemCount: widget.tweets.length,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 2.0,
+        leading: IconButton(
+            icon: const Icon(Icons
+                .person_rounded), //should be changed to google profile icon
+            color: Colors.black,
+            onPressed: () =>
+                {_goToUserProfile(context)}), //button should open to side bar,
+        actions: [
+          Container(
+            width: 260,
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.all(10.0),
+                hintStyle: const TextStyle(
+                  fontFamily: 'RalewayMedium',
+                  fontSize: 14.5,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                hintText: _appBarText,
+              ),
+            ),
+          ),
+
+          IconButton(
+              icon: const Icon(Icons.settings_outlined),
+              color: Colors.black,
+              onPressed: () => {}), //button shoud direct to setings
+        ],
+      ),
+      body: Container(
+        height: mediaQuery.size.height,
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return widget.tweets[index];
+          },
+          itemCount: widget.tweets.length,
+        ),
       ),
     );
   }
