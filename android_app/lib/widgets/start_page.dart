ab9f5be9cc8a6cfc513d35802c5d395d3a5e2e86
Nader-Youhanna <naderyouhanna@gmail.com>
2022-04-02 16:39:13 +0200

    Fixed pixels overflow in start page

--- android_app/lib/widgets/start_page.dart
+++ android_app/lib/widgets/start_page.dart
@@ -23,82 +23,88 @@ class StartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        const SizedBox(),
-        ClipRRect(
-          borderRadius: BorderRadius.circular(18),
-          child: Image.asset(
-            'assets/images/logo_icon.png',
-            width: 120,
-            height: 50,
-            fit: BoxFit.fill,
-          ),
-        ),
-        const SizedBox(
-          height: 90,
-        ),
-        const SizedBox(
-          width: 290,
-          child: Text(
-            'See what\'s happening in the world right now.',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontFamily: 'RalewayMedium',
-              fontSize: 26,
-              fontWeight: FontWeight.bold,
-              //color: Colors.purple,
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 35,
-          width: 265,
-          child: ElevatedButton(
-            child: const Text(
-              'Create Account',
-            ),
-            onPressed: () {
-              goToSignUpPage(context);
-            },
-            style: ElevatedButton.styleFrom(
-              shape: const StadiumBorder(),
-            ),
-          ),
-        ),
-        const SizedBox(
-          height: 170,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            RichText(
-              text: TextSpan(
-                style: DefaultTextStyle.of(context).style,
-                children: <TextSpan>[
-                  TextSpan(
-                    text: 'Have an account already?',
-                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                  ),
-                ],
+            const SizedBox(height: 35),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(18),
+              child: Image.asset(
+                'assets/images/logo_icon.png',
+                width: 120,
+                height: 50,
+                fit: BoxFit.fill,
               ),
             ),
-            TextButton(
-              onPressed: () {
-                goToLoginPage(context);
-              },
-              child: const Text(
-                'Log in',
+            const SizedBox(
+              height: 210,
+            ),
+            const SizedBox(
+              width: 290,
+              child: Text(
+                'See what\'s happening in the world right now.',
+                textAlign: TextAlign.center,
                 style: TextStyle(
-                  fontSize: 16,
+                  fontFamily: 'RalewayMedium',
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            )
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            SizedBox(
+              height: 35,
+              width: 265,
+              child: ElevatedButton(
+                child: const Text(
+                  'Create Account',
+                ),
+                onPressed: () {
+                  goToSignUpPage(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const StadiumBorder(),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 260,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RichText(
+                  text: TextSpan(
+                    style: DefaultTextStyle.of(context).style,
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Have an account already?',
+                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                      ),
+                    ],
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    goToLoginPage(context);
+                  },
+                  child: const Text(
+                    'Log in',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ],
         ),
-      ],
+      ),
     );
   }
 }
