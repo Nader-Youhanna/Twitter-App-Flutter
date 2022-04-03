dbc33ed877ccb2b499243e0224d8a86a2b9773d6
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-04-03 17:46:42 +0200

    Create edit_profile.dart

---
+++ android_app/lib/widgets/user_profile/edit_profile.dart
@@ -0,0 +1,21 @@
+import 'package:android_app/widgets/user_profile/Follow_button.dart';
+import 'package:flutter/material.dart';
+import 'package:android_app/widgets/Tweets/tweet.dart';
+import 'package:android_app/widgets/timeline.dart';
+import 'package:android_app/widgets/user_profile/Show_followers_page.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Edit_Profile extends StatefulWidget {
+  const Edit_Profile({Key? key}) : super(key: key);
+
+  @override
+  State<Edit_Profile> createState() => _Edit_ProfileState();
+}
+
+class _Edit_ProfileState extends State<Edit_Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
