476419f0be1dc67b8059c5d6dc956a59039642a7
Nader-Youhanna <naderyouhanna@gmail.com>
2022-04-05 23:29:25 +0200

    Added sign up with google/facebook buttons

--- android_app/lib/widgets/start_page.dart
+++ android_app/lib/widgets/start_page.dart
@@ -40,7 +40,7 @@ class StartPage extends StatelessWidget {
               ),
             ),
             const SizedBox(
-              height: 210,
+              height: 175,
             ),
             const SizedBox(
               width: 290,
@@ -72,9 +72,43 @@ class StartPage extends StatelessWidget {
                 ),
               ),
             ),
-            const SizedBox(
-              height: 260,
+            const SizedBox(height: 45),
+            //Google and Facebook
+            SizedBox(
+              height: 40,
+              child: Row(
+                children: [
+                  const SizedBox(width: 80),
+                  Image.asset(
+                    'assets/images/google_logo.png',
+                    width: 25,
+                    //height: 70,
+                    //fit: BoxFit.fill,
+                  ),
+                  const SizedBox(width: 15),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Sign Up with Google'),
+                  ),
+                ],
+              ),
             ),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 40,
+              child: Row(
+                children: [
+                  const SizedBox(width: 80),
+                  const Icon(Icons.facebook, size: 26),
+                  const SizedBox(width: 15),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Sign Up with Facebook'),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 150),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
