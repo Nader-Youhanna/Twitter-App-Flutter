8605a9b95c45e415667110817abcbff84012ee4d
ahmedmoh123 <58375227+ahmedmoh1234@users.noreply.github.com>
2022-04-07 23:44:41 +0200

    http get function is now working

--- android_app/lib/functions/http_functions.dart
+++ android_app/lib/functions/http_functions.dart
@@ -15,9 +15,10 @@ Future<List<dynamic>> httpRequestGet(String urlStr, Map? jsonText) async {
   if (jsonText != null) {
     url = url.replace(queryParameters: jsonText as Map<String, String>);
   }
+
   var response = await http.get(url);
+
   print('Response status: ${response.statusCode}');
   //print('Response body: ${response.body}');
-
-  return json.decode(response.body) as List<dynamic>;
+  return json.decode(response.body);
 }
