b8ab3573fd0666f073fc9b2f43af72bc480d242a
Nader-Youhanna <naderyouhanna@gmail.com>
2022-04-22 19:02:29 +0200

    Grouped unit tests in one file (as required)

---
+++ android_app/test/unit_tests.dart
@@ -0,0 +1,43 @@
+import '../lib/widgets/Sign Up/sign_up.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../lib/widgets/Sign Up/choose_password.dart';
+
+void main() {
+  test("Email test", () {
+    String email1 = "naderyouhanna@gmail.com";
+    String email2 = "ahmedmoh123@hotmail.com";
+    String email3 = "habiba@gmail";
+    String email4 = "nada.com";
+
+    SignUp signUp = SignUp();
+
+    bool isValid1 = signUp.isEmailValid(email1);
+    bool isValid2 = signUp.isEmailValid(email2);
+    bool isValid3 = signUp.isEmailValid(email3);
+    bool isValid4 = signUp.isEmailValid(email4);
+
+    expect(isValid1, true);
+    expect(isValid2, true);
+    expect(isValid3, false);
+    expect(isValid4, false);
+  });
+
+  test("Password test", () {
+    String password1 = "123456789";
+    String password2 = "abcdefgh";
+    String password3 = "1234";
+    String password4 = "abcd";
+    ChoosePassword choosePassword =
+        ChoosePassword(username: "", email: "", dob: "");
+
+    bool isValid1 = choosePassword.isPasswordValid(password1);
+    bool isValid2 = choosePassword.isPasswordValid(password2);
+    bool isValid3 = choosePassword.isPasswordValid(password3);
+    bool isValid4 = choosePassword.isPasswordValid(password4);
+
+    expect(isValid1, true);
+    expect(isValid2, true);
+    expect(isValid3, false);
+    expect(isValid4, false);
+  });
+}
