c92f7cd1d95d4368dab19c6d59cd666e0f83299e
Nader-Youhanna <naderyouhanna@gmail.com>
2022-04-24 02:52:24 +0200

    Created Admin View User widget

---
+++ android_app/lib/widgets/Admin View/admin_view_user.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AdminViewUser extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        'Ok',
+      ),
+    );
+  }
+}
--- android_app/lib/widgets/Admin View/admin_view_users.dart
+++ android_app/lib/widgets/Admin View/admin_view_users.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'admin_view_user.dart';
 
 class AdminViewUsers extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: ListView(
+        children: [
+          AdminViewUser(),
+          AdminViewUser(),
+        ],
+      ),
+    );
   }
 }
