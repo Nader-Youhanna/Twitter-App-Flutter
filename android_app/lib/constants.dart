8801accdf2ae820023229f94ed7db2673badda28
ahmedmoh123 <58375227+ahmedmoh1234@users.noreply.github.com>
2022-04-26 01:58:00 +0200

    Added Server IP address to the constants as well as the token to be used for testing

--- android_app/lib/constants.dart
+++ android_app/lib/constants.dart
@@ -1,1 +1,3 @@
-const MY_IP_ADDRESS = '192.168.1.15';
+const MY_IP_ADDRESS = '34.236.108.123:3000';
+const String token =
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNjQ5NzZiYjIwYzdhMjMzNDFhNGUxYiIsImlhdCI6MTY1MDkyNDg3NywiZXhwIjoxNjU5NTY0ODc3fQ.S1ZBOjDv6TcU48AEmn-8nHkgGiasZfj6Id2kk9ocYS4';
