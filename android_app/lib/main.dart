f23f4b5c1358f4c4cbf421a13f6f0adf5e073cfe
Nader-Youhanna <naderyouhanna@gmail.com>
2022-05-14 17:21:52 +0200

    Added Platform detection (Android/Chrome)

--- android_app/lib/main.dart
+++ android_app/lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:android_app/widgets/Push%20Notifications/push_notifications.dart';
-import 'package:android_app/widgets/Settings/ConfirmPassDeactivate.dart';
-import 'package:android_app/widgets/Settings/Deactivate_account.dart';
-import 'package:android_app/widgets/Settings/settings_main.dart';
-import 'package:android_app/widgets/user_profile/edit_profile.dart';
-import 'package:android_app/widgets/user_profile/profile.dart';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:overlay_support/overlay_support.dart';
 import 'package:provider/provider.dart';
@@ -13,7 +9,21 @@ import './providers/google_sign_in.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (defaultTargetPlatform == TargetPlatform.android) {
+    await Firebase.initializeApp();
+    print('Running on Android');
+  } else {
+    await Firebase.initializeApp(
+      // Replace with actual values
+      options: FirebaseOptions(
+        apiKey: "XXX",
+        appId: "XXX",
+        messagingSenderId: "XXX",
+        projectId: "XXX",
+      ),
+    );
+    print("Running on Browser");
+  }
   runApp(MyApp());
 }
 
