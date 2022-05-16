3efb13b1098745c19a61245b4514c6b29cbe217c
Nader-Youhanna <naderyouhanna@gmail.com>
2022-05-16 14:40:33 +0200

    Implemented Google Logout

--- android_app/lib/widgets/side_bar.dart
+++ android_app/lib/widgets/side_bar.dart
@@ -1,3 +1,5 @@
+import 'package:provider/provider.dart';
+import '../providers/google_sign_in.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'Settings/settings_main.dart';
@@ -200,6 +202,9 @@ class SideBar extends StatelessWidget {
           ),
           ListTile(
             onTap: () {
+              final provider =
+                  Provider.of<GoogleSignInProvider>(context, listen: false);
+              provider.logout();
               Navigator.of(context).pop();
               Navigator.of(context).pop();
             },
