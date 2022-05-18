df7bd29e7b616911178e43cb4eb4509845bc7870
habiba <habibaassem@gmail.com>
2022-05-18 02:17:14 +0200

    added users list in admin view

--- android_app/lib/widgets/Admin View/admin_view_user.dart
+++ android_app/lib/widgets/Admin View/admin_view_user.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class AdminViewUser extends StatelessWidget {
@@ -11,23 +13,104 @@ class AdminViewUser extends StatelessWidget {
     this.name,
     this.username,
   );
+
+  AdminViewUser.jsonAdminUser(Map<String, dynamic> jsonAdminUser) {
+    name = jsonAdminUser['name'] as String;
+    username = jsonAdminUser['username'] as String;
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      contentPadding: const EdgeInsets.all(8),
+      isThreeLine: true,
       leading: userImage,
       title: Text(
         name,
         style: const TextStyle(
           fontWeight: FontWeight.bold,
           color: Colors.black,
-          fontFamily: 'RalewayMedium',
         ),
       ),
-      subtitle: Text(
-        username,
-        style: const TextStyle(
-          color: Color.fromARGB(255, 122, 122, 122),
-        ),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            username,
+            textAlign: TextAlign.start,
+            style: const TextStyle(
+              color: Color.fromARGB(255, 122, 122, 122),
+            ),
+          ),
+          Row(mainAxisSize: MainAxisSize.min, children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Color.fromARGB(255, 3, 136, 8),
+                onPrimary: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32.0)),
+                minimumSize: Size(2, 25),
+              ),
+              onPressed: () {},
+              child: Text(
+                'statistics',
+                style: TextStyle(color: Colors.white, fontSize: 11),
+              ),
+            ),
+            SizedBox(
+              width: 4,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32.0)),
+                minimumSize: Size(2, 25),
+              ),
+              onPressed: () {},
+              child: Text(
+                'Profile',
+                style: TextStyle(color: Colors.white, fontSize: 11),
+              ),
+            ),
+            SizedBox(
+              width: 4,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                onPrimary: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32.0)),
+                minimumSize: Size(2, 25),
+              ),
+              onPressed: () {},
+              child: Text(
+                'Ban',
+                style: TextStyle(color: Colors.white, fontSize: 11),
+              ),
+            ),
+            SizedBox(
+              width: 4,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.black,
+                onPrimary: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32.0)),
+                minimumSize: Size(2, 25),
+              ),
+              onPressed: () {},
+              child: Text(
+                'Reports',
+                style: TextStyle(color: Colors.white, fontSize: 11),
+              ),
+            )
+          ]),
+        ],
       ),
     );
   }
--- android_app/lib/widgets/Admin View/admin_view_users.dart
+++ android_app/lib/widgets/Admin View/admin_view_users.dart
@@ -1,16 +1,83 @@
 import 'package:flutter/material.dart';
+import '../../constants.dart';
 import 'admin_view_user.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class AdminViewUsers extends StatelessWidget {
+class AdminViewUsers extends StatefulWidget {
+  @override
+  State<AdminViewUsers> createState() => AdminViewUsersState();
+}
+
+class AdminViewUsersState extends State<AdminViewUsers> {
+  Future<List<AdminViewUser>> _getAdminUsersList() async {
+    List<AdminViewUser> userList = <AdminViewUser>[];
+    var data = [];
+    print("fetching Users in Admin view");
+    var url = Uri.parse("http://${MY_IP_ADDRESS}:3000/users");
+    try {
+      var response = await http.get(
+        url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer ' + token
+        },
+      );
+      if (response.statusCode == 200) {
+        data = json.decode(response.body);
+        userList = data.map((e) => AdminViewUser.jsonAdminUser(e)).toList();
+      } else {
+        print("fetch error");
+      }
+    } on Exception catch (e) {
+      print('error: $e');
+    }
+
+    return userList;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView(
-        children: [
-          AdminViewUser('Nader', 'nido7'),
-          AdminViewUser('Ahmed', 'ahmedMoh123'),
-        ],
-      ),
-    );
+    return FutureBuilder<List<AdminViewUser>>(
+        future: _getAdminUsersList(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Center(child: CircularProgressIndicator());
+            default:
+              List<AdminViewUser> data = snapshot.data!;
+
+              return RefreshIndicator(
+                child: ListView.builder(
+                    clipBehavior: Clip.hardEdge,
+                    padding: const EdgeInsets.all(0),
+                    itemCount: data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        //this container should contain actual notifications not list elements
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black,
+                              offset: Offset(0.0, 1.0), //(x,y)
+                              blurRadius: 1.0,
+                            ),
+                          ],
+                        ),
+                        padding: const EdgeInsets.all(0),
+                        height: 100,
+                        //color: Colors.white,
+                        child: data[index],
+                      );
+                    }),
+                onRefresh: () async {
+                  _getAdminUsersList();
+                  setState(() {});
+                },
+                triggerMode: RefreshIndicatorTriggerMode.anywhere,
+              );
+          }
+        });
   }
 }
