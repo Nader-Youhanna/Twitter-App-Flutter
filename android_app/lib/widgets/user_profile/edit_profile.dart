688586a7fc7429665516105dc3420f87be31d5d9
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-05-19 00:49:59 +0200

    Update edit_profile.dart

--- android_app/lib/widgets/user_profile/edit_profile.dart
+++ android_app/lib/widgets/user_profile/edit_profile.dart
@@ -8,6 +8,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:share_plus/share_plus.dart';
 import 'package:csc_picker/csc_picker.dart';
+import 'package:android_app/constants.dart';
 
 ///class that creates the edit profile settings page (incomplete)
 class Edit_Profile extends StatefulWidget {
@@ -23,10 +24,60 @@ class _Edit_ProfileState extends State<Edit_Profile> {
   String name = "nada";
   String _bio = "this is my bio";
   String country = "Egypt";
-  String city = "cairo";
+  String city = "";
   String website = "www.website.com";
   DateTime birthdate = DateTime.parse("2022-04-25T02:26:31.367Z");
+
+  Future PickDate(BuildContext context) async {
+    final newdate = await showDatePicker(
+      context: context,
+      initialDate: DateTime(2000, 05, 15),
+      firstDate: DateTime(1950),
+      lastDate: DateTime.now(),
+    );
+
+    if (newdate == null) return;
+
+    setState(() {
+      birthdate = newdate;
+    });
+  }
+
+  Future<void> httpRequestPatch() async {
+    var url = Uri.parse('http://${MY_IP_ADDRESS}:3000/settings/profile');
+
+    var response = await http.patch(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ' +
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNjg4ZWNlOWEzNjc3NWIzNDZlNmE2OCIsImlhdCI6MTY1MjY1MzgyMywiZXhwIjoxNjYxMjkzODIzfQ.UPDwftWISguZHasxOJB9F_Uyltgsi2R9azbwgJqzuno',
+      },
+      body: json.encode(
+        <String, String>{
+          "name": "${name}",
+          "bio": "${_bio}",
+          "country": "${country}",
+          "city": "${city}",
+          "website": "${website}",
+          "birthdate": birthdate.toString()
+        },
+      ),
+    );
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+  }
+
   @override
+  void initState() {
+    // TODO: implement initState
+
+    setState(() {
+      // httpRequestGet();
+    });
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
@@ -36,6 +87,7 @@ class _Edit_ProfileState extends State<Edit_Profile> {
         ),
       ),
       home: Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           title: Text(
             'Edit profile',
@@ -62,6 +114,7 @@ class _Edit_ProfileState extends State<Edit_Profile> {
           actions: <Widget>[
             TextButton(
               onPressed: () {
+                httpRequestPatch();
                 Navigator.push(
                     context,
                     MaterialPageRoute(
@@ -76,112 +129,216 @@ class _Edit_ProfileState extends State<Edit_Profile> {
             )
           ],
         ),
-        body: Column(
-          children: <Widget>[
-            Stack(
-              clipBehavior: Clip.none,
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    height: 150,
-                    width: double.infinity,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(
-                              "assets/images/cover_image_sample.jpg"),
-                          fit: BoxFit.cover),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: 30,
-                  top: 115,
-                  child: GestureDetector(
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Stack(
+                clipBehavior: Clip.none,
+                children: <Widget>[
+                  GestureDetector(
                     onTap: () {},
                     child: Container(
-                      padding: EdgeInsets.only(bottom: 20),
-                      alignment: Alignment.topLeft,
-                      child: CircleAvatar(
-                          backgroundColor: Colors.white,
-                          radius: 55,
-                          child: CircleAvatar(
-                            backgroundImage:
-                                new AssetImage('assets/images/user_icon.png'),
-                            radius: 50,
-                            backgroundColor: Colors.grey,
-                          )),
+                      height: 150,
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/cover_image_sample.jpg"),
+                            fit: BoxFit.cover),
+                      ),
                     ),
                   ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 80,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  width: 100,
-                  padding: EdgeInsets.only(top: 20, left: 20, right: 10),
-                  child: Text(
-                    "Name",
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                  Positioned(
+                    left: 30,
+                    top: 115,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        padding: EdgeInsets.only(bottom: 20),
+                        alignment: Alignment.topLeft,
+                        child: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            radius: 45,
+                            child: CircleAvatar(
+                              backgroundImage:
+                                  new AssetImage('assets/images/user_icon.png'),
+                              radius: 40,
+                              backgroundColor: Colors.grey,
+                            )),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    padding: EdgeInsets.only(top: 20, left: 20, right: 10),
+                    child: Text(
+                      "Name",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    alignment: Alignment.centerLeft,
                   ),
-                  alignment: Alignment.centerLeft,
-                ),
-                SizedBox(
-                  width: 320,
-                  child: Form(
-                      child: TextFormField(
-                    textAlignVertical: TextAlignVertical.bottom,
-                    initialValue: "${name}",
-                    autofocus: false,
-                    keyboardType: TextInputType.text,
-                    onChanged: (value) {
-                      setState(() {
-                        if (value != null) {
-                          name = value;
-                        }
-                      });
-                    },
-                  )),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  width: 100,
-                  padding: EdgeInsets.only(top: 20, left: 20, right: 10),
-                  child: Text(
-                    "Bio",
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                  SizedBox(
+                    width: 250,
+                    child: Form(
+                        child: TextFormField(
+                      decoration: InputDecoration(hintText: "Add your name"),
+                      textAlignVertical: TextAlignVertical.bottom,
+                      initialValue: "${name}",
+                      autofocus: false,
+                      keyboardType: TextInputType.text,
+                      onChanged: (value) {
+                        setState(() {
+                          if (value != null) {
+                            name = value;
+                          }
+                        });
+                      },
+                    )),
                   ),
-                  alignment: Alignment.centerLeft,
-                ),
-                SizedBox(
-                  width: 320,
-                  child: Form(
-                      child: TextFormField(
-                    textAlignVertical: TextAlignVertical.bottom,
-                    initialValue: "${_bio}",
-                    autofocus: false,
-                    keyboardType: TextInputType.text,
-                    onChanged: (value) {
-                      setState(() {
-                        if (value != null) {
-                          name = value;
-                        }
-                      });
-                    },
-                  )),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    padding: EdgeInsets.only(top: 20, left: 20, right: 10),
+                    child: Text(
+                      "Bio",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    alignment: Alignment.centerLeft,
+                  ),
+                  SizedBox(
+                    width: 250,
+                    child: Form(
+                        child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Add a bio to your profile"),
+                      textAlignVertical: TextAlignVertical.bottom,
+                      initialValue: "${_bio}",
+                      autofocus: false,
+                      keyboardType: TextInputType.text,
+                      onChanged: (value) {
+                        setState(() {
+                          if (value != null) {
+                            _bio = value;
+                          }
+                        });
+                      },
+                    )),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    padding: EdgeInsets.only(top: 20, left: 20, right: 10),
+                    child: Text(
+                      "Location",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    alignment: Alignment.centerLeft,
+                  ),
+                  Container(
+                    width: 200,
+                    child: CSCPicker(
+                      layout: Layout.horizontal,
+                      countrySearchPlaceholder: "Country",
+                      citySearchPlaceholder: "City",
+                      countryDropdownLabel: "*Country",
+                      cityDropdownLabel: "*City",
+                      dropdownDecoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          color: Colors.white,
+                          border: Border.all(
+                              color: Colors.grey.shade300, width: 1)),
+                      onCountryChanged: (value) {
+                        setState(() {
+                          country = value;
+                        });
+                      },
+                      onCityChanged: (value) {
+                        setState(() {
+                          city = value.toString();
+                        });
+                      },
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    padding: EdgeInsets.only(top: 20, left: 20, right: 10),
+                    child: Text(
+                      "Website",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    alignment: Alignment.centerLeft,
+                  ),
+                  SizedBox(
+                    width: 250,
+                    child: Form(
+                        child: TextFormField(
+                      decoration: InputDecoration(hintText: "Add your website"),
+                      textAlignVertical: TextAlignVertical.bottom,
+                      initialValue: "${website}",
+                      autofocus: false,
+                      keyboardType: TextInputType.text,
+                      onChanged: (value) {
+                        setState(() {
+                          if (value != null) {
+                            website = value;
+                          }
+                        });
+                      },
+                    )),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    padding: EdgeInsets.only(top: 20, left: 20, right: 10),
+                    child: Text(
+                      "Birth date",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    alignment: Alignment.centerLeft,
+                  ),
+                  GestureDetector(
+                      onTap: () {
+                        PickDate(context);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.only(top: 20),
+                        alignment: Alignment.bottomLeft,
+                        child: Text(
+                          '${birthdate.month}/${birthdate.day}/${birthdate.year}',
+                          style: TextStyle(
+                            fontFamily: 'RalewayMedium',
+                          ),
+                        ),
+                      ))
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
